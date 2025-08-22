d62fd10c5db466e5e0ae5173cc5e3e0c5a201e68
Dan <hsammie515@gmail.com>
2025-08-22 11:45:07 +0300

    Добавление верстки, дизайна header

---
+++ src/skills.js
@@ -0,0 +1,11 @@
+export const skillsList = [
+    "JavaScript (React/Vue)",
+    "Python (Django/Flask)",
+    "Java (Spring Boot)",
+    "SQL и базы данных",
+    "Docker и контейнеризация",
+    "Git и системы контроля версий",
+    "REST API разработка",
+    "Тестирование (Jest/Pytest)",
+    "CI/CD настройка"
+]
