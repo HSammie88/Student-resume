cef57d25287e9737667a3c1e3fb0db2a6348210c
Dan <hsammie515@gmail.com>
2025-08-26 11:31:48 +0300

    Проверка заполнения, генерация резюме

--- script.js
+++ script.js
@@ -28,13 +28,33 @@ class FormController{
     constructor(){
         this.navButtons = document.querySelectorAll(".nav-btn")
         this.sections = document.querySelectorAll(".info-section")
+        this.textarea = document.getElementById('softskillsTextarea')
+        this.inputAreas = document.querySelectorAll('.info-input')
+        this.lists = document.querySelectorAll('ul')
+        this.resumeText = this.sections[this.sections.length - 1].querySelectorAll('p')
+        this.resumeButton = this.navButtons[this.navButtons.length - 1]
         this.skillsArray = []
+        this.resumeUnlocker = new Set()
         this.initialSetup()
     }
     initialSetup() {
+        this.textarea.dataset.index = 0
         this.setupNavButtons()
         this.setupSections(0)
         this.setupSkillsButtons()
+        this.setupInputs()
+        this.resumeButton.addEventListener('focus', ()=>{
+            document.getElementById('nameResumeText').innerHTML = this.inputAreas[0].value
+            this.lists[0].innerHTML = ''
+            this.lists[1].innerHTML = ''
+            for(let i = 0; i < this.inputAreas.length - 1; i++)
+                this.resumeText[i].innerText = this.inputAreas[i + 1].value
+            if(this.skillsArray.length !== 0)
+                this.skillsArray.forEach(item => this.addListItem(item, false))
+            else
+                this.addListItem('Нет', false)
+            this.textarea.value.split(', ').forEach(item =>this.addListItem(item, true))
+        })
     }
     setupNavButtons(){
         this.navButtons.forEach((item, index) => {
@@ -85,10 +105,55 @@ class FormController{
                     this.skillsArray.splice(this.skillsArray.indexOf(skill), 1)
                     button.style.cssText = ``
                 }
-                console.log(this.skillsArray)
             })
             skillsPage.appendChild(button)
         })
+    }
+    setupInputs(){
+        this.inputAreas.forEach((item, index) =>{
+            item.addEventListener('blur', ()=>{
+                this.emptinessCheckout(item, index + 1)
+            })
+            item.addEventListener('keydown', (e)=>{
+                if(e.key === 'Enter')
+                    this.emptinessCheckout(item, index + 1)
+            })
+        })
+        this.textarea.addEventListener('blur', ()=>{
+            this.emptinessCheckout(this.textarea, 0)
+        })
+        this.textarea.addEventListener('keydown', (e)=>{
+            if(e.key === 'Enter')
+                this.emptinessCheckout(this.textarea, 0)
+        })
+    }
+    emptinessCheckout(item, index){
+        if(!item.value){
+            item.style.borderBottom = '1px solid red'
+            this.resumeUnlocker.delete(index)
+        }else{
+            item.style.borderBottom = '1px solid green'
+            this.resumeUnlocker.add(index)
+        }
+        if(this.resumeUnlocker.size === this.inputAreas.length + 1){
+            this.resumeButton.disabled = false
+            this.resumeButton.style.cursor = 'pointer'
+        }
+        else{
+            this.resumeButton.disabled = true
+            this.resumeButton.style.cursor = 'not-allowed'
+        }       
+    }
+    addListItem(item, check){
+        const li = document.createElement('li')
+        li.classList.add('final-item')
+        li.innerText = item
+        if(!check){
+            this.lists[0].appendChild(li)
+        }
+        else{
+            this.lists[1].appendChild(li)
+        }
     }
 }
 
